130bd0c8ffac4b2559cf95b356b7a04d91157a3f
Wulfcastle <reiner.wulfcastle@gmail.com>
2014-09-23 23:53:58 +0200

    Adding Login Screen (Front-End)

--- app/src/main/java/com/wulfcastle/missij/MainActivity.java
+++ app/src/main/java/com/wulfcastle/missij/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
@@ -38,6 +39,9 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
 
         // Set up the action bar.
         final ActionBar actionBar = getActionBar();
