b0a902ec06489f984b43bf99538269e3848eee50
Wulfcastle <reiner.wulfcastle@gmail.com>
2014-09-24 12:03:25 +0200

    Added Error Dialog for Sign Up Screen

--- app/src/main/java/com/wulfcastle/missij/SignUpActivity.java
+++ app/src/main/java/com/wulfcastle/missij/SignUpActivity.java
@@ -1,17 +1,58 @@
 package com.wulfcastle.missij;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class SignUpActivity extends Activity {
+
+    protected EditText mUsername;
+    protected EditText mPassword;
+    protected EditText mEmail;
+    protected Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mUsername = (EditText) findViewById(R.id.txtUsername);
+        mPassword = (EditText) findViewById(R.id.txtPassword);
+        mEmail = (EditText) findViewById(R.id.txtEmail);
+        btnSignUp = (Button) findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                String email = mEmail.getText().toString();
+
+                username = username.trim();  // Trimming whitespaces
+                password = password.trim();
+                email = email.trim();
+
+                if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this); // Creating Alert Dialog
+                    builder.setMessage(R.string.sign_up_error_message);
+                    builder.setTitle(R.string.sign_up_error_titles);
+                    builder.setPositiveButton(android.R.string.ok, null);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+
+                } else {
+                    // Create new User
+                }
+            }
+        });
     }
 
 
