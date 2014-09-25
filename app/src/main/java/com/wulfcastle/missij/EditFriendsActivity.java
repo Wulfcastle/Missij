4d7050c049f3b3e49bec48adc5ebc0afcb81f74e
Wulfcastle <reiner.wulfcastle@gmail.com>
2014-09-25 23:20:07 +0200

    Base for Query Functionality

--- app/src/main/java/com/wulfcastle/missij/EditFriendsActivity.java
+++ app/src/main/java/com/wulfcastle/missij/EditFriendsActivity.java
@@ -1,16 +1,32 @@
 package com.wulfcastle.missij;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 
-public class EditFriendsActivity extends Activity {
+public class EditFriendsActivity extends ListActivity {
+
+    public static final String TAG = EditFriendsActivity.class.getSimpleName();
+    protected List<ParseUser> mUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
     }
 
@@ -20,6 +36,51 @@ public class EditFriendsActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.edit_friends, menu);
         return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setProgressBarIndeterminateVisibility(true); // Opening Progress Bar
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.orderByAscending(ParseConstants.KEY_USERNAME);
+        query.setLimit(1000);
+        query.findInBackground(new FindCallback<ParseUser>() {
+
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+
+                setProgressBarIndeterminateVisibility(false); // Closing Progress Bar
+
+
+                if (e == null) {
+
+                    mUsers = users;
+
+                    String[] usernames = new String[mUsers.size()];
+                    int i=0;
+                    for(ParseUser user : mUsers) { // Each in loop - Constructor Loop ----> "ParseUser user" is a single user from Parse.com back-end.
+                    // ParseUser user is being extracted from the List<> of ParseUsers (see line 47 ^^^^) and parsed into the array
+                    // For each ParseUser in list ParseUser do the following :
+
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+
+                    // Creating adapter to convert and display String[] usernames in a list
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this, android.R.layout.simple_list_item_checked, usernames);
+                    setListAdapter(adapter);
+
+
+                } else {
+                    Log.e(TAG, e.getMessage());
+                     alert(getString(R.string.error_title)).show();
+
+                }
+            }
+        });
     }
 
     @Override
@@ -33,4 +94,18 @@ public class EditFriendsActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    public AlertDialog alert(String error) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this); // Creating Alert Dialog
+        builder.setMessage(error);
+        builder.setTitle(R.string.sign_up_error_titles);
+        builder.setPositiveButton(android.R.string.ok, null);
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+
+
+
 }
---
+++ app/src/main/java/com/wulfcastle/missij/ParseConstants.java
@@ -0,0 +1,11 @@
+package com.wulfcastle.missij;
+
+/**
+ * Created by Shimal on 2014-09-25.
+ */
+public final class ParseConstants {
+
+    public static final String KEY_USERNAME = "username";
+
+
+}
