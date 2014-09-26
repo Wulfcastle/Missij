84c84ccaa9a2760b976c20b0ad394d9865195fbd
Wulfcastle <reiner.wulfcastle@gmail.com>
2014-09-26 18:39:17 +0200

    Creating Relations Between Users

--- app/src/main/java/com/wulfcastle/missij/EditFriendsActivity.java
+++ app/src/main/java/com/wulfcastle/missij/EditFriendsActivity.java
@@ -15,7 +15,9 @@ import android.widget.ListView;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.List;
 
@@ -24,6 +26,8 @@ public class EditFriendsActivity extends ListActivity {
 
     public static final String TAG = EditFriendsActivity.class.getSimpleName();
     protected List<ParseUser> mUsers;
+    protected ParseRelation<ParseUser> mFriendsRelation; // Variable to declare friendship between two users
+    protected ParseUser mCurrentUser; // Variable to get current user
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +36,7 @@ public class EditFriendsActivity extends ListActivity {
         setContentView(R.layout.activity_edit_friends);
 
 
-        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE); // Get's default list view associated with this activity and set's the Choice Mode for the List View allow multiple choices
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE); // Gets default list view associated with this activity and set's the Choice Mode for the List View allow multiple choices
     }
 
 
@@ -47,23 +51,27 @@ public class EditFriendsActivity extends ListActivity {
     protected void onResume() {
         super.onResume();
 
+        mCurrentUser = ParseUser.getCurrentUser(); // Setting mCurrentUser to the current user (the user selected from ListView ---> The Edit Friends Page)
+
+        // Getting the relation status of the current user & app user - down below :
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION); // Parse.com automatically creates the String key "KEY_FRIENDS_RELATION" in the back-end of Parse.com
+
         setProgressBarIndeterminateVisibility(true); // Opening Progress Bar
 
+        // Below 4 lines of code get the query
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
         query.setLimit(1000);
         query.findInBackground(new FindCallback<ParseUser>() {
 
+
+            // Below method converts the query to a String Array and uses an ArrayAdapter to displays the query to a ListView
             @Override
             public void done(List<ParseUser> users, ParseException e) {
 
                 setProgressBarIndeterminateVisibility(false); // Closing Progress Bar
-
-
                 if (e == null) {
-
                     mUsers = users;
-
                     String[] usernames = new String[mUsers.size()];
                     int i=0;
                     for(ParseUser user : mUsers) { // Each in loop - Constructor Loop ----> "ParseUser user" is a single user from Parse.com back-end.
@@ -71,18 +79,15 @@ public class EditFriendsActivity extends ListActivity {
                     // For each ParseUser in list ParseUser do the following :
 
                         usernames[i] = user.getUsername();
-                        i++;
-                    }
+                        i++;                    }
 
                     // Creating adapter to convert and display String[] usernames in a list
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this, android.R.layout.simple_list_item_checked, usernames);
                     setListAdapter(adapter);
 
-
                 } else {
                     Log.e(TAG, e.getMessage());
                      alert(getString(R.string.error_title)).show();
-
                 }
             }
         });
@@ -104,6 +109,24 @@ public class EditFriendsActivity extends ListActivity {
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
         super.onListItemClick(l, v, position, id);
+        mFriendsRelation.add(mUsers.get(position)); // Adding Friend by getting position of the displayed user from the ListView.
+        // #Take note that this only adds the relation locally, we also need to save the relation to the Parse.com backend
+
+
+        if (getListView().isItemChecked(position)) {
+            // Add Friend
+            mCurrentUser.saveInBackground(new SaveCallback() { // Saving the relation to the Parse.com back-end
+                @Override
+                public void done(ParseException e) {
+                    if (e != null) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                }
+            });
+        } else {
+            // Remove Friend
+            
+        }
     }
 
     public AlertDialog alert(String error) {
--- app/src/main/java/com/wulfcastle/missij/ParseConstants.java
+++ app/src/main/java/com/wulfcastle/missij/ParseConstants.java
@@ -6,6 +6,7 @@ package com.wulfcastle.missij;
 public final class ParseConstants {
 
     public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
 
 
 }
